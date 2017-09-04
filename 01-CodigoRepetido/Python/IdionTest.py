7de83dcc09d812d8fc22d3b1ad2492bab9349b8b
crivera-git <christiannahuelrivera@gmail.com>
2017-09-04 13:35:49 -0300

    Todavía el no resuelve pero esa es la forma de hacerlo

--- 01-CodigoRepetido/Python/IdionTest.py
+++ 01-CodigoRepetido/Python/IdionTest.py
@@ -1,24 +1,24 @@
 #
 # Developed by 10Pines SRL
-# License: 
-# This work is licensed under the 
-# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
-# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, 
+# License:
+# This work is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
 # California, 94041, USA.
-#  
+#
 import unittest
 import time
 
 class CustomerBook:
-    
+
     CUSTOMER_NAME_CAN_NOT_BE_EMPTY = 'Customer name can not be empty'
     CUSTOMER_ALREADY_EXIST = 'Customer already exists'
     INVALID_CUSTOMER_NAME = 'Invalid customer name'
-    
+
     def __init__(self):
         self.customerNames = set()
-    
+
     def addCustomerNamed(self,name):
         #El motivo por el cual se hacen estas verificaciones y se levanta esta excepcion es por motivos del
         #ejercicio - Hernan.
@@ -26,60 +26,65 @@ class CustomerBook:
             raise ValueError(self.__class__.CUSTOMER_NAME_CAN_NOT_BE_EMPTY)
         if self.includesCustomerNamed(name):
             raise ValueError(self.__class__.CUSTOMER_ALREADY_EXIST)
-        
+
         self.customerNames.add(name)
-        
+
     def isEmpty(self):
         return self.numberOfCustomers()==0
-    
+
     def numberOfCustomers(self):
         return len(self.customerNames)
-    
-    def includesCustomerNamed(self,name): 
+
+    def includesCustomerNamed(self,name):
         return name in self.customerNames
-    
+
     def removeCustomerNamed(self,name):
         #Esta validacion mucho sentido no tiene, pero esta puesta por motivos del ejericion - Hernan
         if not self.includesCustomerNamed(name):
             raise KeyError(self.__class__.INVALID_CUSTOMER_NAME)
-        
+
         self.customerNames.remove(name)
+
+    def tomar_tiempo_de_la_funcion_y_devolver_en_ms(self, objeto, funcion, args):
+        timeBeforeRunning = time.time()
+        self.objeto.funcion(*args)
+        timeAfterRunning = time.time()
+        tiempo = (timeAfterRunning - timeBeforeRunning) * 1000
+        return tiempo
 
 class IdionTest(unittest.TestCase):
     def testAddingCustomerShouldNotTakeMoreThan50Milliseconds(self):
         customerBook = CustomerBook()
 
-        timeBeforeRunning = time.time()
-        customerBook.addCustomerNamed('John Lennon')
-        timeAfterRunning = time.time()
+        tiempo = tomar_tiempo_de_la_funcion_y_devolver_en_ms(self, customerBook, addCustomerNamed, 'John Lennon')
 
-        self.assertTrue((timeAfterRunning - timeBeforeRunning) * 1000 < 50)
+        self.assertTrue(tiempo < 50)
 
     def testRemovingCustomerShouldNotTakeMoreThan100Milliseconds(self):
         customerBook = CustomerBook()
         paulMcCartney = 'Paul McCartney'
-        
-        customerBook.addCustomerNamed(paulMcCartney)
-        timeBeforeRunning = time.time()
-        customerBook.removeCustomerNamed(paulMcCartney)
-        timeAfterRunning = time.time()
 
-        self.assertTrue((timeAfterRunning - timeBeforeRunning) * 1000 < 100)
+        customerBook.addCustomerNamed(paulMcCartney)
+
+        tiempo = tomar_tiempo_de_la_funcion_y_devolver_en_ms(customerBook, removeCustomerNamed, paulMcCartney)
+
+        self.assertTrue(tiempo < 100)
+
 
     def testCanNotAddACustomerWithEmptyName(self):
         customerBook = CustomerBook()
-        
+
         try:
             customerBook.addCustomerNamed('')
             self.fail()
         except ValueError as exception:
             self.assertEquals(exception.message,CustomerBook.CUSTOMER_NAME_CAN_NOT_BE_EMPTY)
             self.assertTrue(customerBook.isEmpty())
-                 
+
     def testCanNotRemoveNotAddedCustomer(self):
         customerBook = CustomerBook()
         customerBook.addCustomerNamed('Paul McCartney')
-        
+
         try:
             customerBook.removeCustomerNamed('John Lennon')
             self.fail()
@@ -88,8 +93,6 @@ class IdionTest(unittest.TestCase):
             self.assertTrue(customerBook.numberOfCustomers()==1)
             self.assertTrue(customerBook.includesCustomerNamed('Paul McCartney'))
 
-      
+
 if __name__ == "__main__":
     unittest.main()
-
-
